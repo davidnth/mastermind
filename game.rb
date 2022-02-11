139df58641ed54f31fb3d6aa4f8eccea38ebb6cc
David <david.nguyen.thai.ho@gmail.com>
2022-02-11 17:05:25 +1100

    game now plays 1 full round and exits when player wins or runs out of tries.

--- game.rb
+++ game.rb
@@ -3,45 +3,50 @@
 class Computer
     attr_accessor :code
 
-    def initialize(code)
-        @code = code
+    def initialize(generate_code)
+        @code = generate_code
     end 
 
 end 
 
 class Player
-    attr_accessor :tries
+    attr_accessor :tries, :arr
 
     def initialize
         @tries = 12
+        @arr
     end 
 
-    def guess
-        puts "Enter a 4 digit code."
-        arr = gets.to_i.digits.reverse
-        @tries -= 1 
+    def guess(secret_code)
+        @arr = input
+        @tries -= 1
+        clue(@arr, secret_code)
+        @arr 
     end
-     
-    def clue(arr, code)
+    
+    # feedback on the guess
+    def clue(arr, secret_code)
         string = ''
-        arr.each_with_index do |val, index|
-            string << 'o' if val == code[index] 
-            string << 'x'if code.include? val unless val == code[index] || string.count('x') == code.count(val)
+        @arr.each_with_index do |val, index|
+            string << 'o' if val == secret_code[index] 
+            string << 'x'if secret_code.include? val unless val == secret_code[index] || string.count('x') == secret_code.count(val)
         end 
-        p string.chars.sort.join
+        clue = string.chars.sort.join
+        p clue 
     end 
 
+    
 
 end 
 
-def guess
-    num = loop do
+def input
+    loop do
         puts 'Enter a 4-digit code.'
         num = gets.chomp
 
-        break num if valid_input?(num)
-    end 
-    num.to_i.digits.reverse
+        return num.to_i.digits.reverse if valid_input?(num)
+        puts 'You have incorrectly entered the code. Please try again.'
+    end
 end
 
 def valid_input?(string)
@@ -49,9 +54,33 @@ def valid_input?(string)
 end 
 
 
-def code
+def generate_code
     arr = Array.new(4).map {|k| k = rand(1..6)}
 end
 
-computer = Computer.new(code)
+def game_over?(player, computer)
+    if player.tries == 0
+        puts 'You are out of guesses. Better luck next time!' 
+        return true
+    end
+    if player.clue(player.arr, computer.code) == 'oooo' 
+        puts 'You have successfully cracked the code. Nice!'
+        return true
+    end
+    false 
+end 
+
+
+def play_round(player, computer)
+    loop do 
+        puts "You have #{player.tries} tries remaining."
+        player.guess(computer.code)
+        break if game_over?(player, computer)
+    end 
+end 
+
+computer = Computer.new(generate_code)
 p computer.code
+player = Player.new
+play_round(player, computer)
+
